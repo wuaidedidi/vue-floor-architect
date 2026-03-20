737ed25fc57b0cf2287adf5dc2eb0c039770036d
wuaidedidi <470357714@qq.com>
2026-03-20 18:28:15 +0800

    .1637650216517962:2c10389212d1d934c75d8befb662a4d6_69bd159d9fbf550e429b910a.69bd1e3c7bb7e8c764643a65.69bd1e3b60078d434cc18806:Trae CN.T(2026/3/20 18:15:24)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
+import './style.css';
 import './assets/styles/main.css';
 
 const app = createApp(App);
